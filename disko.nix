509db265caa39df725e54948da84b9c78c23b7b1
Oleg Khrulev <linqur@gmail.com>
2026-09-17 15:27:36 +0200

    added disko config

---
+++ disko.nix
@@ -0,0 +1,39 @@
+{
+  disko.devices = {
+    disk = {
+      master = {
+        type = "disk";
+        device = "/dev/vda";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              type = "EF00";
+              size = "512M";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            
+            root = {
+              size = "100%";
+              content = {
+                type = "luks";
+                name = "crypted";
+                settings.allowDiscards = true;
+                passwordFile = "/tmp/secret.key";
+                content = {
+                  type = "filesystem";
+                  format = "ext4";
+                  mountpoint = "/";
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
